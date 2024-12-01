f9a9cd44d18f13374310252d9fa38bf37d9bab63
Matt Corbin <mcorbin93@gmail.com>
2024-12-01 15:41:55 -0500

    day 1 solutions

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,11 +1,37 @@
 use std::fs;
+use itertools::izip;
+use regex:: Regex;
+
+fn build_lists(input: &str) -> (Vec<isize>, Vec<isize>) {
+    let mut lefts: Vec<isize> = Vec::new();
+    let mut rights: Vec<isize> = Vec::new();
+    let re = Regex::new(r"(\d+)\s+(\d+)").unwrap();
+    for line in input.lines() {
+        let caps = re.captures(line).unwrap();
+        lefts.push(caps[1].parse().unwrap());
+        rights.push(caps[2].parse().unwrap());
+    }
+    (lefts, rights)
+}
 
 fn part1(input: &str) -> usize {
-    0
+    let (mut lefts, mut rights) = build_lists(input);
+    let mut total = 0;
+    lefts.sort();
+    rights.sort();
+    for (left, right) in izip!(&lefts, &rights) {
+        total += left.abs_diff(*right)
+    }
+    total
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let (lefts, rights) = build_lists(input);
+    let mut total = 0;
+    for item in lefts {
+        total += item as usize * rights.iter().filter(|x| **x == item).count()
+    }
+    total
 }
 
 fn main() {
@@ -21,12 +47,12 @@ mod tests {
     #[test]
     fn test_p1() {
         let input = fs::read_to_string("test.txt").expect("test.txt should exist");
-        assert_eq!(0, part1(&input));
+        assert_eq!(11, part1(&input));
     }
 
     #[test]
     fn test_p2() {
         let input = fs::read_to_string("test.txt").expect("test.txt should exist");
-        assert_eq!(0, part2(&input));
+        assert_eq!(31, part2(&input));
     }
 }
