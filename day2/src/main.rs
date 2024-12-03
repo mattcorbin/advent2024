1421a8b4fa35a3ee5fc81989119533460014f4a3
Matt Corbin <mcorbin93@gmail.com>
2024-12-02 20:23:47 -0500

    day 2 solutions

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,11 +1,52 @@
 use std::fs;
 
+fn all_same_direction(levels: &Vec<isize>) -> bool {
+    levels.windows(2).map(|x| x[0] - x[1]).all(|x| x > 0)
+        || levels.windows(2).map(|x| x[0] - x[1]).all(|x| x < 0)
+}
+
+fn step_within(levels: &Vec<isize>) -> bool {
+    levels
+        .windows(2)
+        .map(|x| (x[0] - x[1]).abs())
+        .all(|x| (1 <= x) && (x <= 3))
+}
+
 fn part1(input: &str) -> usize {
-    0
+    let mut total = 0;
+    for line in input.lines() {
+        let levels: Vec<isize> = line
+            .split(" ")
+            .map(|x| x.parse::<isize>().unwrap())
+            .collect();
+        if all_same_direction(&levels) && step_within(&levels) {
+            total += 1;
+        }
+    }
+    total
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let mut total = 0;
+    for line in input.lines() {
+        let levels: Vec<isize> = line
+            .split(" ")
+            .map(|x| x.parse::<isize>().unwrap())
+            .collect();
+        if all_same_direction(&levels) && step_within(&levels) {
+            total += 1;
+        } else if (0..levels.len())
+            .map(|x| {
+                let mut local = levels.clone();
+                local.remove(x);
+                local
+            })
+            .any(|x| all_same_direction(&x) && step_within(&x))
+        {
+            total += 1;
+        }
+    }
+    total
 }
 
 fn main() {
@@ -21,12 +62,12 @@ mod tests {
     #[test]
     fn test_p1() {
         let input = fs::read_to_string("test.txt").expect("test.txt should exist");
-        assert_eq!(0, part1(&input));
+        assert_eq!(2, part1(&input));
     }
 
     #[test]
     fn test_p2() {
         let input = fs::read_to_string("test.txt").expect("test.txt should exist");
-        assert_eq!(0, part2(&input));
+        assert_eq!(4, part2(&input));
     }
 }
